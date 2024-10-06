d301aea6b71d093ddec17413c17a559d89ec4d66
James Volmert <james.volmert@gmail.com>
2024-10-06 16:44:11 -0600

    added filtering and added animation to selection

--- JP-Predators/ContentView.swift
+++ JP-Predators/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
     let predators = Predators()
     @State var searchText: String = ""
     @State var alphabetical: Bool = false
+    @State var currentSelection = PredatorType.all
     
     var filteredDinos: [ApexPredator] {
+        predators.filter(by: currentSelection)
         predators.sort(by: alphabetical)
         return predators.search(for: searchText)
     }
@@ -59,6 +61,17 @@ struct ContentView: View {
                         Image(systemName: alphabetical ? "film" : "textformat")
                             .symbolEffect(.bounce, value: alphabetical)
                         
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection: $currentSelection.animation())  {
+                            ForEach(PredatorType.allCases) { type in
+                                Label(type.rawValue.capitalized, systemImage: type.icon)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
                     }
                 }
             }
--- JP-Predators/Models/ApexPredator.swift
+++ JP-Predators/Models/ApexPredator.swift
@@ -29,21 +29,43 @@ struct ApexPredator: Decodable, Identifiable {
         let sceneDescription: String
     }
     
-    enum PredatorType: String, Decodable {
-        case land
-        case air
-        case sea
-        
-        var background: Color {
-            switch self {
-                
-            case .land:
-                .brown
-            case .air:
-                .teal
-            case .sea:
-                .blue
-            }
+    
+}
+
+enum PredatorType: String, Decodable, CaseIterable, Identifiable {
+    var id: PredatorType {
+        self
+       }
+    case all
+    case land
+    case air
+    case sea
+    
+    var background: Color {
+        switch self {   
+            
+        case .land:
+            .brown
+        case .air:
+            .teal
+        case .sea:
+            .blue
+        case .all:
+            .black
+        }
+    }
+    
+    var icon: String {
+        switch self {
+            
+        case .all:
+            "square.stack.3d.fill"
+        case .land:
+            "leaf.fill"
+        case .air:
+            "wind"
+        case .sea:
+            "drop.fill"
         }
     }
 }
